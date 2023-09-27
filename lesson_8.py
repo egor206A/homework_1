cdd41e6476a0f5a4a1a6d421aae8dfaf52147874
egor206A <87940443+egor206A@users.noreply.github.com>
2023-09-27 22:59:06 +0500

    Create lesson_8.py

---
+++ lesson_8.py
@@ -0,0 +1,12 @@
+не решенные задача на семинаре:
+# Задание №7
+# Прочитайте созданный в прошлом задании csv файл без
+# использования csv.DictReader.
+# Распечатайте его как pickle строку.
+
+
+def reader_csv(csv_file_name: str) -> bytes:
+    """Читает csv файл, отдает """
+    with open(csv_file_name, 'r', encoding='utf-8') as f:
+        reader = f.read()
+        return pickle.dumps(reader)
